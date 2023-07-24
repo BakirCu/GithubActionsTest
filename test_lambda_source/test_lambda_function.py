4b71be40cb8bb7c7891db09c73ffeef229b3841c
Bakir <curicbakir@gmail.com>
2023-07-24 16:46:04 +0200

    add more tests

--- test_lambda_source/test_lambda_function.py
+++ test_lambda_source/test_lambda_function.py
@@ -1,5 +1,12 @@
-from ..lambda_source.lambda_function import capital_case
+from ..lambda_source.lambda_function import capital_case, lambda_handler
 import pytest
 
 def test_capital_case():
     assert capital_case("data") == "Data"
+
+def test_lambda_handler():
+    responce = lambda_handler("","")
+    assert responce == {
+        'statusCode': 200,
+        'body': '"Hello from Lambda!"'
+    }
